ad9685258593a40634d55334452d6d4e8a9e331a
Thomas Huylebroeck <huylebroeckt@gmail.com>
2022-08-09 21:46:32 +0200

    création fonction productContainer dans lequel j'ai créé les éléments html

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,7 @@
 //récupérer les données serveur (fetch)
 async function getProducts() {
     return await fetch("http://localhost:3000/api/products")
-        .then((response) => response.json());
+        .then((response) => response.json())
         .then((data) => { return data });
     //console.log(products);
 }
@@ -12,29 +12,39 @@ async function getProducts() {
 async function main() {
     let products = await getProducts();
     console.log(products);
+    let itemsCard = document.getElementById("items");// = document.querySelector ("#items")
     //parcourir les données serveur (for of) (let product of products permets de séparer chaque ligne de products)
     for (let product of products) {
         console.log(product);
         //créer chaque article par rapport a une ligne des données serveur (createElement appendchild querySelector)
+        productContainer(product, itemsCard);
+
         console.log(product.name);
     }
-    //appel de la fonction principale
-    main();
+}
+//appel de la fonction principale
+main();
 
 
-    // création d'une variable itemsCard pour aller chercher l'id items du html afin de créer de nouveaux éléments
-    let itemsCard = document.getElementById ("items");
+// création d'une variable itemsCard pour aller chercher l'id items du html afin de créer de nouveaux éléments
 
-    // je créé les éléments structurels de ma section
-    // "La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.". Mozdev
-    product.forEach(getProducts) => {
 
-    let productSection = document.createElement("section");
+// je créé les éléments structurels de ma section
+// "La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.". Mozdev
+function productContainer(pProduit, pItemMain) {
     let productId = document.createElement("a");
-    let productContainer = document.createElement("article");
+    productId.href = `./product.html?id=${pProduit._id}`;
+    let productArticle = document.createElement("article");
     let productPicture = document.createElement("img");
+    productPicture.src= pProduit.imageUrl;
     let productName = document.createElement("h3");
     let productDescription = document.createElement("p");
+    productArticle.appendChild(productPicture);
+    productArticle.appendChild(productName);
+    productArticle.appendChild(productDescription);
+    productId.appendChild(productArticle);
+    pItemMain.appendChild(productId);
 }
+
 
 
