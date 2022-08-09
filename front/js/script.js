bb5682adeca50fabfdb4fa0100d451ec47c05ca3
Thomas Huylebroeck <huylebroeckt@gmail.com>
2022-08-09 17:54:20 +0200

    création des éléments structurels de ma section produits

--- front/js/script.js
+++ front/js/script.js
@@ -1,10 +1,12 @@
 //récupérer les données serveur (fetch)
 async function getProducts() {
     return await fetch("http://localhost:3000/api/products")
-        .then((response) => response.json())
+        .then((response) => response.json());
         .then((data) => { return data });
     //console.log(products);
 }
+
+
 
 //fonction principale
 async function main() {
@@ -15,16 +17,24 @@ async function main() {
         console.log(product);
         //créer chaque article par rapport a une ligne des données serveur (createElement appendchild querySelector)
         console.log(product.name);
-
     }
+    //appel de la fonction principale
+    main();
 
+
+    // création d'une variable itemsCard pour aller chercher l'id items du html afin de créer de nouveaux éléments
+    let itemsCard = document.getElementById ("items");
+
+    // je créé les éléments structurels de ma section
+    // "La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.". Mozdev
+    product.forEach(getProducts) => {
+
+    let productSection = document.createElement("section");
+    let productId = document.createElement("a");
+    let productContainer = document.createElement("article");
+    let productPicture = document.createElement("img");
+    let productName = document.createElement("h3");
+    let productDescription = document.createElement("p");
 }
-//appel de la fonction principale
-main();
 
-//création des articles
-let productSelection = document.getElementsById ("items");
-let productArticle = 8;
-for (let i = 0; i < data.length; i++){
 
-}
